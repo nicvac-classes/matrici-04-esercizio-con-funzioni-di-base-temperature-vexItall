54b4064c0f5f0a67522f7544cf16cdaef8434d87
Benny <fraddosio.patti67@gmail.com>
2025-11-17 14:26:55 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -13,13 +13,39 @@ class Esercizio {
     public static record Max(int massimo, int rIdx, int cIdx) { }
 
     // Metodo per calcolare il massimo nella matrice
-    public static Max calcolaMassimo( /*scrivere qui i parametri richiesti dall'esercizio*/ ) {
+    public static Max calcolaMassimo(int[][] M, int nR, int nC) {
         //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+
+        int massimo = 0;
+        int maxR = 0;
+        int maxC = 0;
+
+        for(int i = 0; i < nR; ++i){
+            for(int j = 0; j < nC; ++j){
+                if(M[i][j] > massimo){
+                    massimo = M[i][j];
+                    maxR = i;
+                    maxC = j;
+                }
+            }
+        }
+
+        return new Max(massimo, maxR, maxC);
+
     }
 
     // Metodo per calcolare la media di una colonna
-    public static float calcolaMedia( /*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO        
+    public static float calcolaMedia(int[][] M, int nR, int iC) {
+        
+        float somma = 0;
+        
+        for(int i = 0; i < nR; ++i){
+            somma += M[i][iC];
+        }
+
+        float media = somma / nR;
+
+        return media;
     }
 
     // Metodo per riempire la matrice con valori casuali
@@ -35,6 +61,38 @@ class Esercizio {
 
     public static void main(String args[]) {
         //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+
+        int[][] M = new int[7][5];
+
+        int nR = 7;
+        int nC = 5;
+
+        riempiCasuale(M, nR, nC, 280, 300);
+
+        UtilsMatrice.visualizza(M);
+
+        System.out.println();
+
+        Max max = calcolaMassimo(M, nR, nC);
+
+        System.out.println("Temperatura massima: " + max.massimo + "°");
+        System.out.println("Giorno: " + max.rIdx + "°");
+        System.out.println("Fascia oraria: " + max.cIdx + "°");
+
+        System.out.println();
+
+        float[] Vm = new float[nC];
+
+        for(int j = 0; j < nC; ++j){
+            Vm[j] = calcolaMedia(M, nR, j);
+        }
+
+        System.out.println("Temperature medie per fasce orarie: ");
+        
+        for(int i = 0; i < Vm.length; ++i){
+            System.out.println((i+1) + "° : " + Vm[i]);
+        }
+
     }
 }
 
